d67c10cb0c9f5ceb477fab3a01f7ecc604cd8f43
o_panda_o <emailofpanda@yahoo.com>
2018-11-01 20:44:36 +0530

    CycleDetectionInDirectedGraph   - static import added   - proper documentation added

--- src/DynamicProgramming/KnapsackProblem.java
+++ src/DynamicProgramming/KnapsackProblem.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
+import static java.lang.Math.max;
 
 public class KnapsackProblem{ 
 
@@ -15,7 +16,7 @@ public class KnapsackProblem{
 		
 		/*
 			For each weight you have 2 options either take that weight or 
-			calculate the optimal soution without taking that weight. So,
+			calculate the optimal solution without taking that weight. So,
 			at each step first we see if we can take that weight. If we can 
 			then we find the best among 2 solutions namely, the solution taking
 			into consideration of the weight and the solution without taking into
@@ -23,12 +24,25 @@ public class KnapsackProblem{
 		*/
 		for(int i=0;i<=n;++i){
 			for(int w=0;w<=W;++w){
+				// If there is no item available or no space is there in knapsack then
+				// nothing could be done
 				if(i==0 || w==0)
 					dp[i][w]=0;
+				// If the given weight of the item can't be kept inside the knapsack then
+				// it is better to discard the item and take whatever items are available
+				// excluding that item
 				else if(weight[i-1]>w)
 					dp[i][w]=dp[i-1][w];
+				// If we are including that item into the knapsack then one of the 2
+				// cases could occur:
+				//  1. We are taking that item: Take the value of the item and the
+				//                              maximum value could be obtained excluding
+				//                              that item & including all the item till that
+				//								point
+				//  2. We are not taking that item: Take the optimal value obtained till that item
 				else
-					dp[i][w]=Math.max(dp[i-1][w],values[i-1]+dp[i-1][w-weight[i-1]]);
+					dp[i][w]=max(dp[i-1][w],
+							     values[i-1]+dp[i-1][w-weight[i-1]]);
 			}
 		}
 		return dp[n][W];
@@ -74,7 +88,7 @@ public class KnapsackProblem{
 		// ii) we are calculating the optimal solution without including the weight.
 		// The optimal solution will be the best of both the scenarios
 		else 
-			return  Math.max(values[n-1]+_solveKnapsackProblem_(W-weight[n-1],weight,values,n-1),
+			return  max(values[n-1]+_solveKnapsackProblem_(W-weight[n-1],weight,values,n-1),
 							 _solveKnapsackProblem_(W,weight,values,n-1));
 	} 
 }
