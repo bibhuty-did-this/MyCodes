189941172790255c7635d945579c5354c03817d2
Bibhuti Bhusan Panda <emailofpanda@yahoo.com>
2019-01-16 16:36:50 +0530

    Code_835C_StarSky(to be continued...)

---
+++ src/Code_835C_StarSky.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Code_835C_StarSky{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter out=new PrintWriter(System.out);
+		StringTokenizer in=new StringTokenizer(br.readLine());
+
+		int n=Integer.parseInt(in.nextToken()),q=Integer.parseInt(in.nextToken()),c=Integer.parseInt(in.nextToken());
+
+
+		out.close();
+	}
+}
