52352b05c754adf9ac0c22d16faf90782d6c9dee
bibhuty <bibhuty.nit@gmail.com>
2019-10-17 22:05:07 +0530

    End of lecture

--- src/Java8/DevoxxAndOracleDevelopers/ParallelAndAsynchronousProgrammingWithStreamsWithCompletableFuture/completableFuture/Part6.java
+++ src/Java8/DevoxxAndOracleDevelopers/ParallelAndAsynchronousProgrammingWithStreamsWithCompletableFuture/completableFuture/Part6.java
@@ -1,7 +1,23 @@
 package Java8.DevoxxAndOracleDevelopers.ParallelAndAsynchronousProgrammingWithStreamsWithCompletableFuture.completableFuture;
 
-public class Part6 {
-    public static void main(String[] args) {
+import java.util.concurrent.CompletableFuture;
 
+public class Part6 {
+    public static CompletableFuture<Integer> create(int number){
+        return CompletableFuture.supplyAsync(()->number);
+    }
+    public static void main(String[] args) {
+        create(2)
+            .thenCombine(create(3),Integer::sum)
+            .thenAccept(System.out::println);
+
+        create(2)
+                //.thenApply(Part6::increment) // returns a completable future, so then accept won't work
+                .thenCompose(Part6::increment) // right way to deal with functions returning completable future
+                .thenAccept(System.out::println);
+    }
+
+    public static CompletableFuture<Integer> increment(int num){
+        return CompletableFuture.supplyAsync(()->num+1);
     }
 }
